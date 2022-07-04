8716a114c446dc0ea649b2aab6bbf6d61157142d
Aninsi Sasberg <98389334+Aninsi-Sasberg@users.noreply.github.com>
2022-07-04 13:41:29 +0200

    correct git setup

--- googlefontsPS.ps1
+++ googlefontsPS.ps1
@@ -1,22 +1,24 @@
 $path = $env:USERPROFILE+"\Desktop\GoogleFonts\git"
+$pathgit = $env:USERPROFILE+"\Desktop\GoogleFonts\git\.git"
 $pathto = $env:USERPROFILE+"\Desktop\GoogleFonts\ALLGoogleFonts"
 
 if (!(Test-Path $env:USERPROFILE"\Desktop\GoogleFonts\" )){
     git clone https://github.com/google/fonts $path
+    Set-Location $pathgit
     git remote add fonts https://github.com/google/fonts
 }
 else{
-    Set-Location $env:USERPROFILE"\Desktop\GoogleFonts\git\fonts\.git"
+    Set-Location $pathgit
     git fetch fonts
 }
 
-if (!(Test-Path $pathto )){
+if (!(Test-Path $pathto)){
     New-Item -itemType Directory -Path $pathto
 }
 else{
 }
 
-if (!(Test-Path $path )){
+if (!(Test-Path $path)){
     New-Item -itemType Directory -Path $path
 }
 else{
